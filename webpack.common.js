c5d5f7e89890e5789745c625ccb4f30332d9d2bc
rdrn-abs <rdrn@abstractit.in>
2023-04-20 11:29:43 +0530

    Implemented webpack.

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: '/src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+	},
+	resolve: {
+		alias: {
+			'react': 'preact/compat',
+			'react-dom': 'preact/compat',
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [new CleanWebpackPlugin()],
+};

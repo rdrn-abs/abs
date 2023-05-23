854d966ba5b62472d1bbfaf825c869955f05d55f
rdrn-abs <rdrn@abstractit.in>
2023-05-23 16:29:40 +0530

    Correction for text classes.

--- SPFMeter/src/components/DisplayText.js
+++ SPFMeter/src/components/DisplayText.js
@@ -4,12 +4,10 @@ import SPFManager from '../services/SPFManager';
 const DisplayText = (context) => {
 	const { spf, protection } = SPFManager.findSegment(context);
 
-	return <div className="display-text">
-		<h1 className="spf-subheading ">
+	return <div className="display-text tt-post-single">
+		<h1 className="spf-heading">
 			Sun Protection Factor Meter</h1>
-		<h2 className=" tt-title recipe-title-desktop  ">
-			Click on the dial to find the SPF protection!</h2>
-		<h2 className=" tt-title recipe-title-mobile  ">
+		<h2 className="tt-title">
 			Click on the dial to find the SPF protection!</h2>
 		<h2 className="tt-title-subpages noborder">  SPF {spf} blocks
 			<span className="protection">

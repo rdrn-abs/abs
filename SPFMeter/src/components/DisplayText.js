8c65f5753636d08f4fc1c71edea3525c599b3f5e
rdrn-abs <rdrn@abstractit.in>
2023-05-23 16:54:13 +0530

    More correction on class names.

--- SPFMeter/src/components/DisplayText.js
+++ SPFMeter/src/components/DisplayText.js
@@ -7,13 +7,14 @@ const DisplayText = (context) => {
 	return <div className="display-text tt-post-single">
 		<h1 className="spf-heading">
 			Sun Protection Factor Meter</h1>
-		<h2 className="tt-title">
-			Click on the dial to find the SPF protection!</h2>
 		<h2 className="tt-title-subpages noborder">  SPF {spf} blocks
 			<span className="protection">
 				{ ` ${ protection }` }
 			</span> of the UVB rays.
 		</h2>
+		<h2 className="tt-title">
+			Click on the dial to find the SPF protection!</h2>
+
 	</div>;
 }	;
 

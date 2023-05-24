6dcd147b4be646dc1699f459fdf1f173cb9d92b5
rdrn-abs <rdrn@abstractit.in>
2023-05-24 15:46:39 +0530

    Changed generic App class name to spf-meter class.

--- SPFMeter/src/App.js
+++ SPFMeter/src/App.js
@@ -4,18 +4,23 @@ import DisplayText from './components/DisplayText';
 import DisplayMeter from './components/DisplayMeter';
 
 const App = (context) => {
+	const { config: { maxDialValue }} = context;
+	const half = 0.5;
 	const [state, setState] = useState({
 		localMouse: { x: 100, y: 160 },
 		inputText: 0,
+		dialValue: maxDialValue * half,
 		containerProps: { width: 300, height: 300 },
 	});
 
 	const extendedContext = { ...context, state, setState };
 
 	return (
-		<div className="App spf-container">
-			<DisplayText { ...extendedContext }/>
-			<DisplayMeter { ...extendedContext }/>
+		<div className="spf-meter">
+			<div className="spf-container">
+				<DisplayText { ...extendedContext }/>
+				<DisplayMeter { ...extendedContext }/>
+			</div>
 		</div>);
 };
 

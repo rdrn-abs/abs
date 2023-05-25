e41e9f5c6da6228524ee8bb85e3567c5c3ea182b
rdrn-abs <rdrn@abstractit.in>
2023-05-25 18:17:32 +0530

    Text position change done.

--- SPFMeter/src/components/DisplayText.js
+++ SPFMeter/src/components/DisplayText.js
@@ -7,12 +7,13 @@ const DisplayText = (context) => {
 	return <div className="display-text tt-post-single">
 		<h1 className="spf-heading">
 			Sun Protection Factor Meter</h1>
+		<h6>(Click on the dial to find the SPF protection.)</h6>
 		<h2>  SPF {spf} blocks
 			<span className="protection">
 				{ ` ${ protection }` }
 			</span> of the UVB rays.
 		</h2>
-		<h6>(Click on the dial to find the SPF protection.)</h6>
+
 	</div>;
 }	;
 

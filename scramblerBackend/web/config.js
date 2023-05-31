613d27364b993afd2c836704fcc7dea9b55a091c
rkr-abs <rkr@abstractit.in>
2023-05-31 12:13:53 +0530

    Dusted

--- scramblerBackend/web/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  words: ["wiring", "damascus", "ingredient", "plankton"],
-  shopifyEndpoint: `https://${process.env.REACT_APP_SHOPIFY_SUB_DOMAIN}.myshopify.com/admin/api/2023-04/graphql.json`,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
-  millisecondsPerDay: 86400000,
-  codeLength: 5,
-};
-
-export default config

3bf4f077b0053cf432a6b7656a563a218e7e3d05
rkr-abs <rkr@abstractit.in>
2023-06-05 13:06:50 +0530

    Check scrambledLetters and  request for discount.

--- scrambler/src/components/InputBox.js
+++ scrambler/src/components/InputBox.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ScramblerManager from '../services/scramblerManager';
+import axios from 'axios';
+
+const setDiscountShown = async (context) => {
+	const { state: { scrambler, input }, setState } = context;
+	// eslint-disable-next-line max-len
+	const { data } = await axios.post(`${ process.env.REACT_APP_URL }/custom/api/scrambleWord?logged_in_customer_id=${ process.env.REACT_APP_CUSTOMER_ID }`,
+		{
+			...scrambler.data,
+			word: input,
+		});
+
+	setState((prev) => ({
+		...prev,
+		discountShown: data,
+	}));
+};
 
 const InputHandler = (contextData) => {
 	const { setState, data } = contextData;
@@ -15,12 +31,19 @@ const InputHandler = (contextData) => {
 };
 
 const InputBox = (context) => {
-	const { state } = context;
+	const { state: { input, wordObject: { scrambledLetters }}} = context;
+
+	useEffect(() => {
+		const isAllLetterMatch = scrambledLetters
+			.every((scrambledLetter) => scrambledLetter.entered);
+
+		isAllLetterMatch && setDiscountShown(context);
+	}, [scrambledLetters]);
 
 	return (
 		<input
 			className="text-input"
-			value={ state.input }
+			value={ input }
 			placeholder="Click here to start"
 			pattern="A-Z"
 			onChange={ (event) =>

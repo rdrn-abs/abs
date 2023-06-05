a456a28d7ede4bb6950e67d373c1ec89f0d54296
rkr-abs <rkr@abstractit.in>
2023-06-05 15:42:20 +0530

    Introduced retry component.

---
+++ scrambler/src/components/Retry.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Retry = () =>
+	<div className="retry-card">
+		<div className="retry-title">You are almost there.</div>
+		<button className="retry-btn">Retry</button>
+	</div>;
+
+export default Retry;

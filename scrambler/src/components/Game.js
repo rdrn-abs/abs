66a3cfa747b73b488f627394384b1569f1b30d78
rkr-abs <rkr@abstractit.in>
2023-06-06 13:52:12 +0530

    Introduce loading component.

--- scrambler/src/components/Game.js
+++ scrambler/src/components/Game.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import InputBox from './InputBox';
 import DisplayWord from './DisplayWord';
+import Loading from './Loading';
 
-const Game = (context) => <div className="game-card">
-	<h1 className="tt-description">
-		Unscramble the word and claim your reward!
-	</h1>
-	<InputBox { ...context }/>
-	<DisplayWord { ...context }/>
-</div>;
+const Game = (context) => {
+	const { state: { isLoading }} = context;
+
+	return !isLoading
+		? <div className="game-card">
+			<h1 className="tt-description">
+				Unscramble the word and claim your reward!
+			</h1>
+			<InputBox { ...context }/>
+			<DisplayWord { ...context }/>
+		</div>
+		: <Loading { ...context }/>;
+};
 
 export default Game;
---
+++ scrambler/src/components/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Loading = () =>
+	<div className="loading-card">
+		<div className="loading-title">Loading...</div>
+	</div>;
+
+export default Loading;

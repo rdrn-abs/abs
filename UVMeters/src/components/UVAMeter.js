90d028181fb6aabf3ec6748e1efde3b8665ede52
rdrn-abs <rdrn@abstractit.in>
2023-06-09 16:23:06 +0530

    Moved images to public.

--- UVMeters/src/components/UVAMeter.js
+++ UVMeters/src/components/UVAMeter.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ImageMap from './ImageMap';
 import DisplayUVAText from './DisplayUVAText';
-import parent from '../images/UVADial.png';
-import needle from '../images/UVANeedleCrop.png';
 
-const UVAMeter = (context) => <div className="display-meter">
-	<DisplayUVAText { ... { ...context, data: { type: 'UVA' }} }/>
-	<ImageMap { ...{
-		...context,
-		data: { parent: parent, needle: needle, type: 'UVA' },
-	} }
-	/>
-	<h4 className="spf-label">SPF</h4>
-</div>;
+const UVAMeter = (context) => {
+	const parent = '/images/UVADial.png';
+	const needle = '/images/UVANeedle.png';
+
+	return <div className="display-meter">
+		<DisplayUVAText { ...{ ...context, data: { type: 'UVA' }} }/>
+		<ImageMap { ...{
+			...context,
+			data: { parent: parent, needle: needle, type: 'UVA' },
+		} }
+		/>
+		<h4 className="spf-label">SPF</h4>
+	</div>;
+};
 
 export default UVAMeter;
--- UVMeters/src/components/UVBMeter.js
+++ UVMeters/src/components/UVBMeter.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ImageMap from './ImageMap';
 import DisplayUVBText from './DisplayUVBText';
-import parent from '../images/dial11Crop.png';
-import needle from '../images/needle11CropWidth.png';
 
-const UVBMeter = (context) => <div className="display-meter">
-	<DisplayUVBText { ... { ...context, data: { type: 'UVB' }} }/>
-	<ImageMap { ...{
-		...context,
-		data: { parent: parent, needle: needle, type: 'UVB' },
-	} }
-	/>
-	<h4 className="spf-label">SPF</h4>
-</div>;
+const UVBMeter = (context) => {
+	const parent = '/images/UVBDial.png';
+	const needle = '/images/UVBNeedle.png';
+
+	return <div className="display-meter">
+		<DisplayUVBText { ...{ ...context, data: { type: 'UVB' }} }/>
+		<ImageMap { ...{
+			...context,
+			data: { parent: parent, needle: needle, type: 'UVB' },
+		} }
+		/>
+		<h4 className="spf-label">SPF</h4>
+	</div>;
+};
 
 export default UVBMeter;

c7e55bde92ed70ed458883f95de71f2326cc6c61
rkr-abs <rkr@abstractit.in>
2023-06-09 13:39:20 +0530

    Added discount collection url.

--- scrambler/src/components/Discount.js
+++ scrambler/src/components/Discount.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const Discount = (context) => {
-	const { state: { discount: { data: { data }}}} = context;
-	const { discountCodeBasicCreate: { codeDiscountNode: { codeDiscount:
-		{ title, codes: { nodes }}}}} = data;
-	const [first] = nodes;
+	const { state: { discount: { data:
+		{ data: { discountCodeBasicCreate, collectionUrl }}}}} = context;
+
+	const { codeDiscountNode: { codeDiscount:
+		{ title, codes: { nodes }}}} = discountCodeBasicCreate;
+
+	const [{ code }] = nodes;
 
 	return <div className="game-card">
 		<h3 className="discount-title">{title}</h3>
 		<div>Use this coupon and claim your reward.</div>
-		<div className="discount-code">{first.code}</div>
+		<div className="discount-code">{code}</div>
+		<div><a href={ collectionUrl }>Check Discount products</a></div>
 		<p>Valid for 24 hours.</p>
 		<h4>Play more!! Come back tomorrow for the new discount.</h4>
 	</div>;
--- scrambler/src/components/DisplayWord.js
+++ scrambler/src/components/DisplayWord.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const DisplayWord = ({ state: { wordObject: { scrambledLetters }}}) =>
 	<div className="display-word">
-
 		{scrambledLetters
 			&& scrambledLetters.map((item, index) =>
 				<span key={ index } className={ `test-word ${ item.entered ? 'highlighted' : '' }` }>

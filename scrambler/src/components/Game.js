9f8745c2e4bef5ab9536072f481b9c9fc6d4c53e
rkr-abs <rkr@abstractit.in>
2023-06-12 11:18:06 +0530

    Introduced GameContent component.

--- scrambler/src/components/Game.js
+++ scrambler/src/components/Game.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import InputBox from './InputBox';
-import DisplayWord from './DisplayWord';
 import Loading from './Loading';
+import GameContent from './GameContent';
 
 const Game = (context) => {
 	const { state: { isLoading }} = context;
 
 	return !isLoading
-		? <div className="game-card">
-			<h1 className="tt-description">
-				Unscramble the word and claim your reward!
-			</h1>
-			<InputBox { ...context }/>
-			<DisplayWord { ...context }/>
-		</div>
+		? <GameContent { ...context }/>
 		: <Loading { ...context }/>;
 };
 
---
+++ scrambler/src/components/GameContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputBox from './InputBox';
+import DisplayWord from './DisplayWord';
+
+const GameContent = (context) =>
+	<div className="game-card">
+		<h1 className="tt-description">
+			Unscramble the word and claim your reward!
+		</h1>
+		<InputBox { ...context }/>
+		<DisplayWord { ...context }/>
+	</div>;
+
+export default GameContent;

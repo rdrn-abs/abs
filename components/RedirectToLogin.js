a7667d8309195662c66de740216f30f7245b975d
rkr-abs <rkr@abstractit.in>
2023-06-19 10:10:29 +0530

    Adding style.

--- components/RedirectToLogin.js
+++ components/RedirectToLogin.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./redirectToLogin.css"
 
 const RedirectToLogin = ({ config: { loginLink } }) => (
-  <div>
-    <div>Login to play game</div>
-    <a href={loginLink}>Login</a>
+  <div className="redirect-login-card">
+    <div className="redirect-login-title">Login to play game</div>
+    <a href={loginLink} className="redirect-btn-login">Login</a>
   </div>
 );
 

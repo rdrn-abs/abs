41571f59c330a104d0c14cc089e1445a2bf620fa
rkr-abs <rkr@abstractit.in>
2023-06-22 13:49:49 +0530

    Refactored.

--- components/RedirectToLogin.js
+++ components/RedirectToLogin.js
@@ -3,7 +3,7 @@ import "./redirectToLogin.css"
 
 const RedirectToLogin = ({ config: { loginLink } }) => (
   <div className="redirect-login-card">
-    <div className="redirect-login-title">Login to play game</div>
+    <div className="redirect-login-title">Experience the ultimate scramble thrill! Unleash your full gaming potential by logging in and let the adventure begin!</div>
     <a href={loginLink} className="redirect-btn-login">Login</a>
   </div>
 );

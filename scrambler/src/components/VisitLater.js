9728a4a04bd921dcf1f4e93e9ceb260de6f2d9d0
rkr-abs <rkr@abstractit.in>
2023-07-10 12:54:21 +0530

    Implemented copy discount code feature.

--- scrambler/src/components/VisitLater.js
+++ scrambler/src/components/VisitLater.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import RedirectToHome from './RedirectToHome';
+import { peek } from '@laufire/utils/debug';
+
+const win = `You've conquered the scramble challenge!
+Take a breather and prepare for another thrilling scramble session in just`;
+
+const lose = `Master the scramble, but heed the rule of three -
+tomorrow holds fresh chances for you to reign supreme!`;
+
+const messages = {
+	win,
+	lose,
+};
+
+const copyDiscount = async (ref) => {
+	const copyText = ref.current.textContent;
+
+	await navigator.clipboard.writeText(copyText)
+		.catch(() => peek('error'));
+};
 
 const VisitLater = (context) => {
-	const { state: { scrambler: { error: { nextAvailableAt }}}} = context;
+	const { state: { scrambler: { error: { nextAvailableAt, code }}}} = context;
+	const ref = useRef(null);
 
 	return (
 		<div className="visitLater-card">
 			<p className="visitLater-title">
-				You&apos;ve conquered the scramble challenge!
-				Take a breather and prepare for another thrilling
-				scramble session in just <b className="visitLater-hours">
-					{nextAvailableAt}
-				</b> hours!
+				{messages[code]}
+				<b className="visitLater-hours">{nextAvailableAt}</b> hours!
 			</p>
+			<h1>DiscountCode</h1>
+			<button onClick={ () => copyDiscount(ref) }>Copy</button>
 			<RedirectToHome { ...context }/>
 		</div>
 	);

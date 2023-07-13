8718ed3aee69413c57df6c06943504fd041ac319
rdrn-abs <rdrn@abstractit.in>
2023-07-13 18:25:28 +0530

    Dusted

--- scrambler/src/core/config.js
+++ scrambler/src/core/config.js
@@ -1,8 +1,6 @@
 
 const config = {
-	loginLink: process.env.REACT_APP_LOGIN_URL,
-	appUrl: process.env.REACT_APP_URL,
-	cid: process.env.REACT_APP_CUSTOMER_ID,
+	milliSeconds: 1000,
 };
 
 export default config;
